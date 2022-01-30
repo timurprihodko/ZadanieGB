6f29d3017518aba094d151e168ff076aafb67b6b
timurPrik <timur.prikhodko@gmail.com>
2022-01-30 20:36:51 +0200

    Создали миссив

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string[] array =
+            {
+                "abc",
+                "12",
+                "asd",
+                "dd",
+                "123",
+                "123d"
+            };
