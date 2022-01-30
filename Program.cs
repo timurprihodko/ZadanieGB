12da1bee176ad4b2d2f3f5f6d55dc0e5bb254b89
timurPrik <timur.prikhodko@gmail.com>
2022-01-30 20:42:24 +0200

    Создание цикла

--- Program.cs
+++ Program.cs
@@ -18,4 +18,12 @@ namespace ConsoleApp1
 
             var result = new string[array.Length];
             var realSize = 0;
-
+            foreach (var value in array)
+            {
+                if (value.Length <= 3)
+                {   
+                    result[realSize] = value;
+                    realSize++;
+                }
+                else continue;
+            }
